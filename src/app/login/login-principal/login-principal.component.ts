1c8317d54c68962f79d079cbd109b669078d581f
elifio <elifioroldan@hotmail.com>
2023-11-19 23:42:27 -0500

    Login

--- src/app/login/login-principal/login-principal.component.ts
+++ src/app/login/login-principal/login-principal.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-principal',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login-principal.component.css']
 })
 export class LoginPrincipalComponent {
+
+  constructor(private router:Router){
+
+  }
+
+  enviar(){
+    this.router.navigate(["/pasajero"])
+  }
 
 }
